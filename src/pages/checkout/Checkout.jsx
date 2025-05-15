b75816d0b48e0bd5a189e5ad2754da0b262c2cd0
Robiul1711 <ashiq.web.dev@gmail.com>
2025-05-15 16:37:02 +0600

    responsive done

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,36 +1,47 @@
-import SectionBanner from '@/components/home/SectionBanner';
-import React from 'react';
-import checkoutBanner from '@/assets/images/checkoutBanner.png'
-import AnySpecialRequests from '@/components/checkoutComponents/AnySpecialRequests';
-import SunshineAndSoul from '@/components/checkoutComponents/SunshineAndSoul';
-import BookingCancellationPolicy from '@/components/checkoutComponents/BookingCancellationPolicy';
-import PriceDetails from '@/components/checkoutComponents/PriceDetails';
+import SectionBanner from "@/components/home/SectionBanner";
+import checkoutBanner from "@/assets/images/checkoutBanner.png";
+import AnySpecialRequests from "@/components/checkoutComponents/AnySpecialRequests";
+import SunshineAndSoul from "@/components/checkoutComponents/SunshineAndSoul";
+import BookingCancellationPolicy from "@/components/checkoutComponents/BookingCancellationPolicy";
+import PriceDetails from "@/components/checkoutComponents/PriceDetails";
 const Checkout = () => {
-    return (
-        <div>
-            <div className='relative'>
-<img src={checkoutBanner} alt="" className='w-full' />
-<div className='absolute top-0 left-0 w-full section-padding-x flex justify-between gap-6'>
-    <div className='space-y-7 w-[70%]'>
-   <SunshineAndSoul />
-<BookingCancellationPolicy />
-    </div>
-    <div className='w-[30%]'>
-        <PriceDetails />
-    </div>
-</div>
+  return (
+    <div>
+      <div
+        className="bg-no-repeat bg-cover bg-center pt-10"
+        style={{ backgroundImage: `url(${checkoutBanner})` }}
+      >
+        {/* <img src={checkoutBanner} alt="" className="w-full h-full" /> */}
+        <div className=" w-full section-padding-x flex flex-col xlg:flex-row  justify-between gap-6">
+          <div className="space-y-7  xlg:w-[70%]">
+            <div className="flex flex-col xlg:flex-col gap-4">
+              <SunshineAndSoul />
+              <div className="xlg:hidden">
+                <PriceDetails />
+              </div>
+              <BookingCancellationPolicy />
             </div>
-            <div className='section-padding-x max-w-[70%] w-full space-y-6'>
-                <div className='flex items-center justify-between  bg-[#FEF7DA] p-4 rounded-xl '>
-                    <h1 className=''>Any issue to complete your booking?</h1>
-                    <button className='border border-primary px-4 py-2 rounded-md'>Click here</button>
-                </div>
-                <AnySpecialRequests />
-
+            <div className="flex flex-col xmd:flex-row w-full xlg:flex-col items-start gap-4">
+              <div className="flex items-start sm:items-center justify-between w-full bg-[#FEF7DA] p-4 rounded-xl ">
+                <h1 className="text-xs xxs:text-sm sm:text-base">
+                  Any issue to complete your booking?
+                </h1>
+                <button className="border border-primary px-4 py-2 text-xs xxs:text-sm sm:text-base rounded-md">
+                  Click here
+                </button>
+              </div>
+              <AnySpecialRequests />
             </div>
-              <SectionBanner />
+          </div>
+          <div className="xlg:w-[30%] hidden xlg:block">
+            <PriceDetails />
+          </div>
         </div>
-    );
+      </div>
+
+      <SectionBanner />
+    </div>
+  );
 };
 
 export default Checkout;
